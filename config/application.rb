1426293fd23697f7ea6a767a2c0fbd845c823ac2
sun-dev-it <daiphatle123@gmail.com>
2026-05-02 16:26:36 +0700

    disable solid cable

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module WebG
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    
+    config.action_cable.adapter = :async
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
